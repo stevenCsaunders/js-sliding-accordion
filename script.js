63232e0dc546201172859c75dcaad2d895290f5b
Steven <steve0183@gmail.com>
2019-07-09 17:23:45 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const accordion = document.querySelectorAll('.slide-menu .slide-btn');
+//MULTIPLT MENU ITEMS OPEN AT A TIME
+for(let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function() {
+
+    let child = accordion[i].nextElementSibling;
+
+    if(!this.classList.contains('active')){
+      this.classList.add('active');
+      child.style.maxHeight = child.scrollHeight + 'px';
+    } else {
+      this.classList.remove('active');
+      child.style.maxHeight = '0px'
+    }
+  });
+};
+
+
+
+
+
+// //SINGLE MENU ITEM OPEN AT AT TIME
+// for(let i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener('click', function() {
+//     for(var i = 0; i < accordion.length; i++) {
+
+//     let child = accordion[i].nextElementSibling;
+
+//     if(!this.classList.contains('active') && this === accordion[i]){
+//       this.classList.add('active');
+//       child.style.maxHeight = child.scrollHeight + 'px';
+//     } else {
+//       this.classList.remove('active');
+//       child.style.maxHeight = '0px'
+//     }
+//   };
+//   });
+// };
